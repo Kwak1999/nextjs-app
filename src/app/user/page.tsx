04984ec9001523160a3a9cf84559178b6b194f69
HuiBeom <gmlqja7913@daum.net>
2025-11-06 23:53:38 +0900

    getServerSession

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import {getServerSession} from "next-auth";
+import {authOptions} from "@/pages/api/auth/[...nextauth]";
 
-const UserPage = () => {
+const UserPage = async () => {
+
+    const session = await getServerSession(authOptions);
+    console.log("session", session);
     return (
         <div>
             UserPage
