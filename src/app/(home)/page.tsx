bbababb98132504b0704acd56064852d53bb81b4
HuiBeom <gmlqja7913@daum.net>
2025-11-18 23:08:17 +0900

    홈페이지 데이터 생성

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,20 @@
+import getProducts, {ProductsParams} from "@/app/actions/getProducts";
+
+interface HomeProps {
+    searchParams: ProductsParams
+}
+
+
+export default async function Home({searchParams}: HomeProps) {
+
+    const products = await getProducts(searchParams)
+
+    console.log(products)
+
+    return (
+      <main>
+          누구나 볼 수 있는 페이지
+      </main>
+  )
+
+}
