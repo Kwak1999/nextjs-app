5952ad733997c03f41f323aa03ef055c937cb96e
HuiBeom <gmlqja7913@daum.net>
2025-11-22 23:55:36 +0900

    HomePage UI update

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,8 @@
 import getProducts, {ProductsParams} from "@/app/actions/getProducts";
 import Container from "@/app/components/Container";
 import EmptyState from "@/app/components/EmptyState";
+import ProductCard from "@/app/components/ProductCard";
+import getCurrentUser from "@/app/actions/getCurrentUser";
 
 interface HomeProps {
     searchParams: ProductsParams
@@ -9,27 +11,33 @@ interface HomeProps {
 
 export default async function Home({searchParams}: HomeProps) {
 
-    const products = await getProducts(searchParams)
+    const params = await searchParams;  // ✅ Promise 풀기
+    const products = await getProducts(params);  // 이제 안전함
 
-    console.log(products)
+    const currentUser = await getCurrentUser();
+
+    console.log('@@@',products)
 
     return (
       <Container>
-        {/*    Categories     */}
-        {/*<EmptyState />*/}
-
+        {/*    Category     */}
           {
               products?.data.length === 0
               ?
               <EmptyState />
               :
               <>
-                <div>
-ddd
+                <div className='grid grid-cols-1 gap-8 pt-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6'>
+                    {products.data.map((product) => (
+                        <ProductCard
+                            currentUser = {currentUser}
+                            key={product.id}
+                            data={product}
+                        />
+                    ))}
                 </div>
               </>
           }
       </Container>
   )
-
 }
